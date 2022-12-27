a7b97dbf62d715477d7dd948c0dab9f3830feebc
Timothée Hirt <astreptocoque@gmail.com>
2022-12-27 15:45:12 +0100

    corrected 2.1 by adding Ts

--- rocket_project/Deliverable_2_1/Deliverable_2_1.m
+++ rocket_project/Deliverable_2_1/Deliverable_2_1.m
@@ -1,3 +1,5 @@
+
+Ts = 1/20;
 rocket = Rocket(Ts);
 [xs, us] = rocket.trim() % Compute steady−state for which 0 = f(xs,us)
 sys = rocket.linearize(xs, us) % Linearize the nonlinear model about trim point
