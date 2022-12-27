cfba5721bceee0f5c492918ead2b759cd418bbf8
Y99dr451l <matthias@kockisch.eu>
2022-12-27 21:51:47 +0100

    added deliverable 5.1

--- rocket_project/Deliverable_5_1/Deliverable_5_1.m
+++ rocket_project/Deliverable_5_1/Deliverable_5_1.m
@@ -11,3 +11,35 @@ rmpath(fullfile('Deliverable_6_1'))
 clear; close all;
 
 %% This file should produce all the plots for the deliverable
+Ts = 1/20;
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys x, sys y, sys z, sys roll] = rocket.decompose(sys, xs, us);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+
+% Merge four sub−system controllers into one full−system controller
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+
+% Evaluate once and plot optimal open−loop trajectory,
+% pad last input to get consistent size with time and state
+x0 = zeros(12,1);
+ref4 = [2 2 2 deg2rad(40)]';
+[u, T_opt, X_opt, U_opt] = mpc.get_u(x0, ref4);
+U_opt(:,end+1) = nan;
+ph = rocket.plotvis(T_opt, X_opt, U_opt, ref4); % Plot as usual
+
+% Setup reference function
+ref = @(t_, x_) ref_EPFL(t_);
+rocket.mass = 1.794; % Manipulate mass for simulation
+
+% Simulate
+Tf = 30;
+[T, X, U, Ref] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref);
+
+% Visualize
+rocket.anim_rate = 10; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
